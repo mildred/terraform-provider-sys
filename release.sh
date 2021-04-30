e8fecdc67ca60d8d8d78518e9ec73a8edddfad61
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2021-04-30 21:14:09 +0200

    Update release script

--- release.sh
+++ release.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-if [[ ${1:0:1} != v ]]; then
-  echo "Usage $0 vX.Y.Z [TAG_MESSAGE]"
+tag=true
+
+while true; do
+  case "$1" in
+    --no-tag)
+      tag=false
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+if tag && [[ ${1:0:1} != v ]]; then
+  echo "Usage $0 [--no-tag] vX.Y.Z [TAG_MESSAGE]"
   exit 1
 fi
 
@@ -15,7 +29,9 @@ set -xe
 eval $(pass show sites/github.com | grep GITHUB_TOKEN)
 GPG_FINGERPRINT=01230FD4CC29DE17
 
-git tag -s -u $GPG_FINGERPRINT -m "$msg" "$ver"
+if $tag; then
+  git tag -s -u $GPG_FINGERPRINT -m "$msg" "$ver"
+fi
 
 goreleaser release --rm-dist
 
