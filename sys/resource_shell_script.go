f14fa044636132a7918c8b2893149184ef467b71
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2021-11-05 09:53:00 +0100

    Update shell script to add a make argument + better errors

--- sys/resource_shell_script.go
+++ sys/resource_shell_script.go
@@ -20,32 +20,44 @@ func resourceShellScript() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"working_directory": {
+				Description: "Working directory where to run the script",
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
 			},
 			"shell": {
+				Description: "Shell to use",
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
 				Default:  "/bin/sh",
 			},
+			"make": {
+				Description: "Script to construct the resource (does not read the value)",
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
 			"create": {
+				Description: "Script to construct the resource, in addition to `make`. Must output on the standard output the resource id (used to determine if the resource needs to be reconstructed).",
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
 			},
 			"read": {
+				Description: "Script that reads the resource id on the standard output",
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
 			},
 			"delete": {
+				Description: "Script to delete the resource",
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
 			},
 			"filename": {
+				Description: "Filename created by the resource, can be used to avoid implementing `read`. The file is removed on resource deletion.",
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
@@ -69,7 +81,9 @@ func resourceShellScriptRead(d *schema.ResourceData, _ interface{}) error {
 	if ok {
 		id, err := resourceShellScriptRun(cwd, shell, script.(string))
 		d.SetId(id)
-		return err
+		if err != nil {
+			return fmt.Errorf("cannot execute read script, %v", err)
+		}
 	} else if filename, ok := d.GetOk("filename"); ok {
 		id, err := checksumFile(filename.(string))
 		if err != nil && !os.IsNotExist(err) {
@@ -81,29 +95,48 @@ func resourceShellScriptRead(d *schema.ResourceData, _ interface{}) error {
 }
 
 func resourceShellScriptCreate(d *schema.ResourceData, _ interface{}) error {
-	script, ok := d.GetOk("create")
+	script_make, okm := d.GetOk("make")
+	script, okc := d.GetOk("create")
 	filename, okf := d.GetOk("filename")
-	_, okr := d.GetOk("read")
+	script_read, okr := d.GetOk("read")
 	cwd := d.Get("working_directory")
 	shell := d.Get("shell").(string)
-	if ok && okr {
+
+	scriptname := "create"
+	if script.(string) == "" {
+		script = script_read
+		scriptname = "read"
+	}
+
+	if okm {
+		_, err := resourceShellScriptRun(cwd, shell, script_make.(string))
+		if err != nil {
+			return fmt.Errorf("cannot execute make script, %v", err)
+		}
+	}
+
+	if okc && okr {
 		id, err := resourceShellScriptRun(cwd, shell, script.(string))
 		d.SetId(id)
-		return err
-	} else if ok && okf {
+		if err != nil {
+			return fmt.Errorf("cannot execute %s script, %v", scriptname, err)
+		}
+	} else if okc && okf {
 		_, err := resourceShellScriptRun(cwd, shell, script.(string))
 		if err != nil {
-			return err
+			return fmt.Errorf("cannot execute %s script, %v", scriptname, err)
 		}
 		id, err := checksumFile(filename.(string))
 		if err != nil && !os.IsNotExist(err) {
 			return fmt.Errorf("cannot checksum file, %v", err)
 		}
 		d.SetId(id)
-	} else if ok {
+	} else if okc {
 		_, err := resourceShellScriptRun(cwd, shell, script.(string))
+		if err != nil {
+			return fmt.Errorf("cannot execute %s script, %v", scriptname, err)
+		}
 		d.SetId("1")
-		return err
 	} else {
 		d.SetId("1")
 	}
